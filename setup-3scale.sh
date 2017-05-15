f7d6ab42586186ba8b7768e54a508baf7adab773
Nicolas Grenié <nicolas.grenie@gmail.com>
2017-05-15 12:15:04 -0700

    setup script

---
+++ setup-3scale.sh
@@ -0,0 +1,8 @@
+# 1. create a new Service
+# 2. create a new member
+# 3. activate member
+# 4. add member to service, application, analytics
+
+npm install node-3scale-cli
+mv ./assets/credentials.json ~/.3cale/
+cat ~/.3cale/credentials.json
