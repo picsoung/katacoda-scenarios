cef723cc782819c70146a3e2af6cbfa5180945b0
Nicolas Grenié <nicolas.grenie@gmail.com>
2017-05-15 14:20:49 -0700

    setup

--- hello-world/setup-3scale.sh
+++ hello-world/setup-3scale.sh
@@ -4,15 +4,17 @@
 # 4. add member to service, application, analytics
 
 mkdir ~/.3scale/
-touch ~/.3scale/credentials.json
-echo '{' ~/.3scale/credentials.json
-echo '  "threescale": {' ~/.3scale/credentials.json
-echo '   "id": "commandline",' ~/.3scale/credentials.json
-echo '   "provider_key": "df8411bfaaad72f68581c7814772b474",' ~/.3scale/credentials.json
-echo '   "service_id": 2555417742291,' ~/.3scale/credentials.json
-echo '   "access_token": "5ddeb96c1481b9c5482d3407f867c6c5159aafe7c5c5454e096f45703d33f9e7"' ~/.3scale/credentials.json
-echo '  }' ~/.3scale/credentials.json
-echo '}' ~/.3scale/credentials.json
+cd ~/.3scale/
+touch credentials.json
+echo '{' credentials.json
+echo '  "threescale": {' credentials.json
+echo '   "id": "commandline",' credentials.json
+echo '   "provider_key": "df8411bfaaad72f68581c7814772b474",' credentials.json
+echo '   "service_id": 2555417742291,' credentials.json
+echo '   "access_token": "5ddeb96c1481b9c5482d3407f867c6c5159aafe7c5c5454e096f45703d33f9e7"' credentials.json
+echo '  }' credentials.json
+echo '}' credentials.json
+npm install node-3scale-cli
 
 # mv credentials.json ~/.3cale/.
 # npm install node-3scale-cli
