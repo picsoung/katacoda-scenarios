59b453bfb76a06823d0863d438f75150658118c6
Nicolas Grenié <nicolas.grenie@gmail.com>
2017-05-18 23:24:03 -0700

    change image to node6 and display default user info

--- hello-world/setup-3scale.sh
+++ hello-world/setup-3scale.sh
@@ -1,9 +1,11 @@
-KIKIRANDOM = $RANDOM
+npm install node-3scale-cli
 
-#create service
-output=$(curl -X POST \
-  https://commandline-admin.3scale.net/admin/api/services.xml \
-  -H 'content-type: application/x-www-form-urlencoded' \
-  -d "provider_key=df8411bfaaad72f68581c7814772b474&name=DefaultAPI-${KIKIRANDOM}")
-
-serviceId=$(sed -ne '/service_id/{s/.*<service_id>\(.*\)<\/service_id>.*/\1/p;q;}' <<< "$output")
+# KIKIRANDOM= $RANDOM
+#
+# #create service
+# output=$(curl -X POST \
+#   https://commandline-admin.3scale.net/admin/api/services.xml \
+#   -H 'content-type: application/x-www-form-urlencoded' \
+#   -d "provider_key=df8411bfaaad72f68581c7814772b474&name=DefaultAPI-${KIKIRANDOM}")
+#
+# serviceId=$(sed -ne '/service_id/{s/.*<service_id>\(.*\)<\/service_id>.*/\1/p;q;}' <<< "$output")
