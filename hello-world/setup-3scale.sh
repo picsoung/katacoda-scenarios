a442a083bbfd8d70c6541569e08f7704cfae8a69
Nicolas Grenié <nicolas.grenie@gmail.com>
2017-05-18 23:35:30 -0700

    clean script

--- hello-world/setup-3scale.sh
+++ hello-world/setup-3scale.sh
@@ -1,11 +1,1 @@
 npm install node-3scale-cli
-
-# KIKIRANDOM= $RANDOM
-#
-# #create service
-# output=$(curl -X POST \
-#   https://commandline-admin.3scale.net/admin/api/services.xml \
-#   -H 'content-type: application/x-www-form-urlencoded' \
-#   -d "provider_key=df8411bfaaad72f68581c7814772b474&name=DefaultAPI-${KIKIRANDOM}")
-#
-# serviceId=$(sed -ne '/service_id/{s/.*<service_id>\(.*\)<\/service_id>.*/\1/p;q;}' <<< "$output")
